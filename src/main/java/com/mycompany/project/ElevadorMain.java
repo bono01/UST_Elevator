b1a8149a99aadf5d474159d2d3a7b607e3588a0a
823217710 <823217710@PTALI20309.animaedu.intranet>
2024-04-15 21:40:25 -0300

    versão inicial.

---
+++ src/main/java/com/mycompany/project/ElevadorMain.java
@@ -0,0 +1,18 @@
+package com.mycompany.project;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Victor Mendes Bono
+ * RA: 823217710
+ */
+public class ElevadorMain {
+    public static void main(String [] args){
+        TelaElevador tela = new TelaElevador();
+        tela.setVisible(true);
+    }
+}
